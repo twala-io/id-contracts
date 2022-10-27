a8fa8727104a6a67d7fb7c7082412c4ef2458f98
Alexander Paul Quinit <paulquinit@gmail.com>
2022-10-28 04:05:49 +0800

    Update contract

--- NFT/NFT.sol
+++ NFT/NFT.sol
@@ -5,17 +5,21 @@ import "./token/ERC721/ERC721.sol";
 import "./token/ERC721/extensions/ERC721Enumerable.sol";
 import "./token/ERC721/extensions/ERC721URIStorage.sol";
 import "./token/ERC721/extensions/ERC721Burnable.sol";
-import "./access/Ownable.sol";
+import "./access/AccessControl.sol";
 import "./utils/Counters.sol";
 
-contract NonFungible is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract NonFungible is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl {
     using Counters for Counters.Counter;
 
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("NonFungible", "NFT") {}
+    constructor() ERC721("NonFungible", "NFT") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
 
-    function safeMint(address to, string memory uri) public onlyOwner {
+    function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
@@ -47,7 +51,7 @@ contract NonFungible is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnab
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721, ERC721Enumerable, AccessControl)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
